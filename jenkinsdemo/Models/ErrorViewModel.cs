3964a6c83cc2dfb6bd48569093308d7078ad60ff
simao <simao@ARI1109>
2025-01-06 14:35:54 +0000

    Add project files.

---
+++ jenkinsdemo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace jenkinsdemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
